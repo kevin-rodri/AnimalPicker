5f4ecc5ed9753797bae44c2c9929aa9d03a7a446
kevin-rodri <kr399880@gmail.com>
2022-02-21 20:36:53 -0500

    have user pick between a dog or cat

---
+++ picker.py
@@ -0,0 +1,5 @@
+userInput = input("What animal would you like to see: Dog or Cat")
+if userInput.lower() == "dog":
+    print("Dog ASCII ART HERE")
+elif userInput.lower() == "cat":
+    print("Cat ASCII ART HERE")
