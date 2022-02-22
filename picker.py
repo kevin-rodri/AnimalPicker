8ca0b8423eff4f3fa929e4ea42962a8dc9b28a73
kevin-rodri <kr399880@gmail.com>
2022-02-21 21:25:21 -0500

    replaced original printlns to ascii art for each animal

--- picker.py
+++ picker.py
@@ -3,10 +3,10 @@ while keepGoing == True:
     print("What animal would you like to see: Dog or Cat")
     userInput = input()
     if userInput.lower() == 'dog':
-        print("Dog ASCII ART HERE")
+        print("^..^      /\n/_/\\_____/\n   /\\   /\\\n  /  \\ /  \\")
         keepGoing = False
     elif userInput.lower() == 'cat':
-        print("Cat ASCII ART HERE")
+        print(" /\\_/\\\n( o o )\n==_Y_==\n  `-\'")
         keepGoing = False
     else:
         print("Invalid option")
